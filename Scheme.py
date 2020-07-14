c375e9e7b9588efebbd3054abd0faf010ce00c17
Oscar Stomberg <oscar@stomberg.us>
2020-07-14 16:03:25 -0400

    Fixed numpy list usage and simplified grouping process

--- Scheme.py
+++ Scheme.py
@@ -1,4 +1,5 @@
 import math
+import random
 import numpy as np
 from PIL import Image
 
@@ -11,8 +12,8 @@ def colordif(pix1, pix2):
 
 
 def getavg(data):
-    avg = np.round(np.average(data, axis=0)).astype(np.int)
-    return avg.tolist()
+    avg = np.round(np.average(data, axis=0))
+    return avg
 
 
 def tohex(col):
@@ -42,26 +43,25 @@ def binpixels(img, coords, rad):
 
 def binimage(img, rad):
     w = math.floor(img.width / (rad + 1))
-    print(img.width, w, w * (rad + 1))
     h = math.floor(img.height / (rad + 1))
     binned = Image.new('RGB', (w, h), 'white')
     print('Initiating array...')
-    pxmp = np.array([[[0, 0, 0]] * w] * h, dtype=np.int8)
+    pxmp = np.array([[[0, 0, 0, 0]] * w] * h, dtype=np.int16)
     print('Populating array...')
     for y in range(h):
         for x in range(w):
             px = binpixels(img, (y * (rad + 1), x * (rad + 1)), rad)
+            px = np.append(px, 0).astype(np.int16)
             pxmp[y][x] = px
             binned.putpixel((x, y), (px[0], px[1], px[2]))
-        print(y, h)
+        print(h - y)
     binned.save('Binned.png')
     return pxmp
 
 
 def preparr(arr):
     print('Flattening array...')
-    px = np.reshape(arr, (count, 3))
-    px = px.astype(np.int16)
+    px = np.reshape(arr, (count, 4))
     print('Ensuring no negative values...')
     for p in px:
         for x in range(3):
@@ -70,24 +70,23 @@ def preparr(arr):
 
 
 def groupx(pxls, thresh):
-    print('Initiating index array...')
-    added = [False] * count
+    pix = np.copy(pxls)
     print('Starting grouping process...')
     grps = []
-    while False in added:
-        p = added.index(False)
-        px = pxls[p]
+    while len(pix > 0):
+        print(len(pix))
+        p = random.randint(0, len(pix) - 1)
+        px = pix[0]
         grps.append([])
         x = 0
-        while len(pxls) > x:
-            p = pxls[x]
-            if not added[x]:
-                dif = colordif(px, p)
-                if dif < thresh:
-                    grps[-1].append(p.tolist())
-                    added[x] = True
-            x += 1
-        print(added.count(True))
+        while len(pix) > x:
+            p = pix[x]
+            dif = colordif(px, p)
+            if dif < thresh:
+                grps[-1].append(p.tolist())
+                pix = np.delete(pix, x, axis=0)
+            else:
+                x += 1
     return grps
 
 
@@ -96,7 +95,7 @@ def merge(grps):
     rgb = []
     hx = []
     for grp in grps:
-        c = getavg(grp)
+        c = getavg(grp).astype(np.int8)
         rgb.append(c)
         hx.append(tohex(c))
     return rgb, hx
@@ -116,7 +115,9 @@ def output(rgb, hx):
 
 
 path = '/usr/share/backgrounds/'
-file = 'Sand Dune.png'
+# path = '/home/oscar/Pictures/Gimp/Exports/'
+file = 'Mechanized Metropolis.png'
+# file = 'Test Image.png'
 
 print('Opening image...')
 image = Image.open(path + file)
@@ -129,7 +130,7 @@ count = pixmap.shape[0] * pixmap.shape[1]
 
 pixels = preparr(pixmap)
 
-groups = groupx(pixels, 24)
+groups = groupx(pixels, 12)
 
 rgbs, hexs = merge(groups)
 
